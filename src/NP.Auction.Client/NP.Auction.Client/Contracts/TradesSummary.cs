b8bca90d99cb463cfa4704164884cda991a87561
geniy21 <89915069+geniy21@users.noreply.github.com>
2022-03-25 11:20:35 +0200

    Adding company name to order repsonse and additional fields for block trades (#6)

--- src/NP.Auction.Client/NP.Auction.Client/Contracts/TradesSummary.cs
+++ src/NP.Auction.Client/NP.Auction.Client/Contracts/TradesSummary.cs
@@ -8,10 +8,15 @@
         public string AreaCode { get; set; }
         public string AuctionId { get; set; }
         public string OrderId { get; set; }
+        public string CompanyName { get; set; }
         public string Portfolio { get; set; }
         public string CurrencyCode { get; set; }
         public string UserId { get; set; }
         public OrderType OrderType { get; set; }
+        public string Name { get; set; }
+        public string ExclusiveGroup { get; set; }
+        public string LinkedTo { get; set; }
+        public bool IsSpreadBlock { get; set; }
         public List<Trade> Trades { get; set; }
     }
 
